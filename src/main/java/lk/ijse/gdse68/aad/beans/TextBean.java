fe3f702a7306be4dd662c00b439a6f9cff7c4565
ApsaraWitharana <asachini095@gmail.com>
2024-08-04 13:06:41 +0530

    update-  PreDestroy method in work

--- src/main/java/lk/ijse/gdse68/aad/beans/TextBean.java
+++ src/main/java/lk/ijse/gdse68/aad/beans/TextBean.java
@@ -1,5 +1,6 @@
 package lk.ijse.gdse68.aad.beans;
 
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.*;
 import org.springframework.context.ApplicationContext;
@@ -24,6 +25,14 @@ public class TextBean implements BeanNameAware, BeanFactoryAware, ApplicationCon
         System.out.println("going");
     }
 
+    @PreDestroy
+    public void  customerDestroyTest(){
+        System.out.println("man ynoo with customer destroy"); // lastly check krgnn mek spring Destroy ekk newe
+
+        //PreDestroy vs Destroy = close weddi resource cline krgnn use we. //
+        // reader/db connection close krgnn spring framework work eke hammen Destroy ek nanntm custom eke hamben PreDestroy
+        //@PreDestroy = method in work krnn use we pojo ek newe // life circle method
+    }
     @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("ITS 2024 -adad");
@@ -31,6 +40,6 @@ public class TextBean implements BeanNameAware, BeanFactoryAware, ApplicationCon
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-
+        System.out.println("applicationContext is "+ applicationContext);
     }
 }
